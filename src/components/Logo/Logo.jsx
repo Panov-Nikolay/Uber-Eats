4cdae45cce4d513b7be6037f48d64ccf791a14c3
Nikolay <pan0v-nikolay@yandex.com>
2023-08-23 21:42:52 +0300

    mobile width

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -4,16 +4,16 @@ import styles from './Logo.module.css'
 
 const scrollTop = () => {
 	window.scrollTo({
-		top: 0, 
-		behavior: 'smooth'
+		top: 0,
+		behavior: 'smooth',
 	})
 }
 
-export const Logo = ({src}) => {
+export const Logo = ({ src }) => {
 	return (
 		<div className={styles.Logo}>
-			<Link to={'/restaurants'} onClick={scrollTop}>
-				<img src={src} alt="Uber Eats" />
+			<Link to={'/'} onClick={scrollTop}>
+				<img src={src} alt='Uber Eats' />
 			</Link>
 		</div>
 	)
